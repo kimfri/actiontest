ec326f59e737c2c34df4b97337daa753d21198a6
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2023-05-18 23:01:00 +0200

    what about now

--- src/main/java/org/kimfri/Main.java
+++ src/main/java/org/kimfri/Main.java
@@ -1,19 +1,6 @@
 package org.kimfri;
 
 public class Main {
-  /**
-   * This is the main method, and it will start the project.
-   *
-   * @param args String[] containing all args from cmd-line
-   */
-  public static void main(final String[] args) {
-    Main main = new Main();
-    main.firstMethod();
-  }
-
-  final void firstMethod() {
-    add(1, 2);
-  }
 
   /**
    * Method will add first and second argument.
