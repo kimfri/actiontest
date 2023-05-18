0870be5ef403351613e42b5e676a23a062623ea7
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2023-05-18 23:12:32 +0200

    google code style, take 2

--- src/test/java/org/kimfri/MainTest.java
+++ src/test/java/org/kimfri/MainTest.java
@@ -1,9 +1,9 @@
 package org.kimfri;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
